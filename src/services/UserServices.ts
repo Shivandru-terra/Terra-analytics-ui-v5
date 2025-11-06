c54b2a89cd03bcb0b525bd00ee67d90a6b894895
Shivandru-terra <shivandru@letsterra.com>
2025-11-06 11:19:52 +0530

    apiRouteFixed

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -4,7 +4,7 @@ import { UserType } from "@/types/userType";
 class UserServices {
   async fetchUsers(): Promise<UserType[]> {
     try {
-      const userUrl = generalFunctions.createUrl("users/");
+      const userUrl = generalFunctions.createUrl("users");
       const res = await fetch(userUrl);
       if (!res.ok) {
         throw new Error("Failed to fetch users");
