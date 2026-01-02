bf46d8a618918809aa4124043f06118eb9d80b21
Shivandru-terra <shivandru@letsterra.com>
2026-01-02 11:51:09 +0530

    [fix] made external

--- src/lib/generalFuntion.ts
+++ src/lib/generalFuntion.ts
@@ -1,8 +1,8 @@
 class GeneralFunctions {
 
-    // private BASE_URL = "https://fastapi-service-v5-427230556695.asia-south2.run.app";
+    private BASE_URL = "https://fastapi-service-v5-427230556695.asia-south2.run.app";
     // private BASE_URL = "https://fastapi-service-v4-427230556695.asia-south2.run.app";
-    private BASE_URL = "http://localhost:3500";
+    // private BASE_URL = "http://localhost:3500";
     createUrl(apiString: string): string {
         const url = `${this.BASE_URL}/${apiString}`;
         return url;
